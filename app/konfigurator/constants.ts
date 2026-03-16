267298c2054b97dc125848e78a61f599b551b5cf
daniel schamott <daniel-schamott@dci.org>
2026-03-16 16:07:08 +0100

    hölle

---
+++ app/konfigurator/constants.ts
@@ -0,0 +1,17 @@
+export const GARMENT_IMAGES = [
+  '/workwear/1.png',
+  '/workwear/2.png',
+  '/workwear/3.png',
+  '/workwear/4.png',
+] as const;
+export const DEFAULT_GARMENT_INDEX = 1;
+
+export const PREVIEW_DROP_ID = 'preview-drop';
+export const ZONE_DROP_PREFIX = 'zone:';
+
+export const INITIAL_ZONE_RECT = {
+  x: 56,
+  y: 29,
+  w: 18,
+  h: 10,
+};
